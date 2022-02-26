a8d32dfab2409fe774ee85ee1cc7be269bb5ef6f
fred5229 <frederik_godske@hotmail.dk>
2022-02-26 13:07:16 +0100

    mt19937 produces only positive numbers

--- MiniProject1/TupleGenerator.cpp
+++ MiniProject1/TupleGenerator.cpp
@@ -11,7 +11,7 @@ vector<tuple<long long, long long>> TupleGenerator::GenerateTuples(int count) {
     random_device rd;  // Will be used to obtain a seed for the random number engine
     // Payload needs to be 16 bytes (tuple of long long)
     mt19937_64 gen(rd()); // Standard mersenne_twister_engine seeded with rd()
-    uniform_int_distribution<> dis(LONG_LONG_MIN, LONG_LONG_MAX);
+    uniform_int_distribution<> dis(0,ULONG_LONG_MAX);
     for (int i = 0; i < count; i++)
     {
         result.emplace_back(make_tuple(dis(gen), dis(gen)));
